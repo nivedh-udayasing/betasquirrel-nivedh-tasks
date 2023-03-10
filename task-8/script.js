7dce96593253fd85ce539f867856bcf2044c799e
nivedh-udayasing <nivedhsing16@gmail.com>
2023-03-10 23:16:58 +0530

    simple calculator

---
+++ task-8/script.js
@@ -0,0 +1,17 @@
+function display(value) {
+  document.getElementById("result").value += value;
+}
+function calculate() {
+  var p = document.getElementById("result").value;
+  var q = eval(p);
+  document.getElementById("result").value = q;
+}
+function clearscreen() {
+  document.getElementById("result").value = "";
+}
+function backspace() {
+  document.getElementById("result").value = document
+    .getElementById("result")
+    .value.toString()
+    .slice(0, -1);
+}
